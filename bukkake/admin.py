2460982661fdb878268fa95e971c51045868627b
Délita Makanda <delita.makanda@gmail.com>
2020-05-31 22:42:31 +0200

    refactor api bukkake

--- bukkake/admin.py
+++ bukkake/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from bukkake.models import ChatMessage, Bukkake, Comment
+from bukkake.forms import AdminChatMessageForm
 
 # Register your models here.
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    form = AdminChatMessageForm
+    list_display = ('user', 'message', 'message_html', 'updated', 'created',)
+
+class BukkakeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created', 'updated']
+    list_filter = ['created', 'updated']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['commented_by', 'created_on']
+
+admin.site.register(Bukkake, BukkakeAdmin)
+admin.site.register(Comment, CommentAdmin)
--- bukkake/forms.py
+++ bukkake/forms.py
@@ -1,11 +1,15 @@
 import ssl
+import bleach
+import markdown
+import re
 
 from urllib.request import urlopen, Request
+from django.utils.html import escape
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
+from django.core.validators import RegexValidator
 from django import forms
-from .models import Bukkake
-from .models import Comment
+from bukkake.models import Bukkake, ChatMessage, Comment
 from django.conf import settings
 
 class BukkakeCreateForm(forms.ModelForm):
@@ -53,3 +57,36 @@ class CommentModelForm(forms.ModelForm):
             'body': 'Write a comment'
         }
         fields = ('body',)
+
+
+class AdminChatMessageForm(forms.ModelForm):
+
+    class Meta:
+        model = ChatMessage
+        fields = ['user', 'message', 'message_html']
+
+    def clean(self):
+        message = self.cleaned_data['message']
+
+        message_html = escape(message)
+
+        urlRegex = re.compile(
+                u'(?isu)(\\b(?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)[^\\s()<'
+                u'>\\[\\]]+[^\\s`!()\\[\\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019])'
+            )
+        processed_urls = list()
+        for obj in urlRegex.finditer(current_message):
+            old_url = obj.group(0)
+            if old_url in processed_urls:
+                continue
+            processed_urls.append(old_url)
+            new_url = old_url
+            if not old_url.startswith(('https://', 'http://')):
+                new_url = 'http://' + new_url
+            new_url = '<a href="'+ new_url + '">' + new_url + '</a>'
+            message_html = message_html.replace(old_url, new_url)
+
+        self.cleaned_data['message_html'] = message_html
+
+        return self.cleaned_data
+
