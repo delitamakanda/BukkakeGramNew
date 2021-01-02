3931649099ff4f715aa877253fb5528c24e1dc6e
Délita Makanda <delita.makanda@gmail.com>
2021-01-02 17:11:59 +0100

    fix duplicate

--- bukkake/consumers.py
+++ bukkake/consumers.py
@@ -15,7 +15,6 @@ from channels_presence.models import Room
 from channels_presence.decorators import touch_presence
 from django.dispatch import receiver
 from channels_presence.signals import presence_changed
-from channels import Group
 
 @channel_session_user_from_http
 def chat_connect(message):
